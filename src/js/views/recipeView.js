abead5798b21b601fda7e1c34a0f470ad82488c9
Goran <temeljkovitch.goran@gmail.com>
2024-09-30 14:52:37 -0400

    changed fraction library

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from '../../img/icons.svg';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 import View from './View';
 
 class RecipeView extends View {
@@ -130,7 +130,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''
+        ingredient.quantity ? fracty(ingredient.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ingredient.unit}</span>
